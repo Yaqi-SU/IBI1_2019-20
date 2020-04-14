ea3d510bd4b2cdf97c649f72f9567170e8247448
Yaqi-SU <yaqi.19@intl.zju.edu.cn>
2020-04-14 14:47:45 +0800

    mystery code added

---
+++ Practical5/mystery_code.py
@@ -0,0 +1,24 @@
+# What does this piece of code do?
+# Answer: This code is to find an n between 1 and 100 which is not divisible by any integer between 2 and the ceiling of n**(0.5) plus 1. 
+
+# Import libraries
+# randint allows drawing a random number, 
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+p=False
+while p==False:
+    p=True
+    n = randint(1,100) #n is drawn randomly between 1 and 100.
+    u = ceil(n**(0.5)) #u is the ceiling of n**(0.5)
+    for i in range(2,u+1): #i consists of all integers between 2 and u+1 including 2 but without u+1
+        if n%i == 0: #if n is divisible by i, the loop will continue.
+            p=False
+
+
+     
+print(n) 
